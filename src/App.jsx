48a38e53088656408a297365f6cdc497342ba020
Friday Diamond <diamondfriday90@gmail.com>
2025-11-11 12:38:52 +0100

    feat: add Header component and integrate lucide-react icons; update Home page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <div>
-      <p className="bg-green-400">Hello World</p>
-      <div className="w-10 h-10 bg-red-400 hover:bg-green-400 transition"></div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Search } from "lucide-react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-primary-50 px-20 py-24">
+      <div className="flex flex-col gap-10 items-center justify-center text-center">
+        {/* content */}
+        <div className="flex flex-col gap-4 items-center">
+          <span className="bg-primary-100 text-primary-700 px-3 py-1 rounded-2xl font-medium">Our blog</span>
+          <h2 className="text-primary-900 font-semibold text-5xl">Resources and insight</h2>
+          <p className="text-primary-700 text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        {/* Search bar */}
+        <form action="" className="flex items-center gap-2 bg-white border-gray-300 border py-3 px-4 rounded-lg">
+          <Search size={15}/>
+          <input type="search" placeholder="Search..." className="outline-name" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
